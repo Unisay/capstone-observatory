1ba2476794bca1e1020228ccc75fa36ae1b66893
Yuriy Lazarev <unisay@gmail.com>
2017-05-07 20:31:22 +0200

    Faster visualization

--- src/main/scala/observatory/Visualization.scala
+++ src/main/scala/observatory/Visualization.scala
@@ -11,7 +11,7 @@ import scala.collection.parallel.mutable.ParArray
 object Visualization {
 
   val earthRadiusMeters = 6372800
-  val powerParameter = 4.0
+  val powerParameter = 2.0
 
   def greatCircleDistance(a: Location, b: Location): Double = {
     val dLat = (b.lat - a.lat).toRadians
@@ -29,23 +29,18 @@ object Visualization {
     predictTemperaturePar(temperatures.toParArray)(location)
 
   def predictTemperaturePar(temps: ParArray[(Location, Double)])(location: Location): Double = {
-    val (closestTemperature, minDistanceMeters) = temps.foldLeft((Double.MaxValue, Double.MaxValue)) {
-      case (same @ (_, minDistance), (nextLocation, nextTemp)) =>
-        val nextDistance = greatCircleDistance(location, nextLocation)
-        if (nextDistance < minDistance) (nextTemp, nextDistance) else same
+    val (numerator, denominator) = temps.foldLeft((0.0, 0.0)) {
+      case ((num, den), (loc, temp)) =>
+        val gcd = greatCircleDistance(location, loc)
+        val wix = 1.0 / pow(gcd, powerParameter)
+        val _num = num + wix * temp
+        val _den = den + wix
+        if (gcd < 1000)
+          return temp // early abort fold
+       else (_num, _den)
     }
-    if (minDistanceMeters <= 1000) {
-      closestTemperature
-    } else {
-      def wi(x: Location, xi: Location): Double = 1.0 / pow(greatCircleDistance(x, xi), powerParameter)
-      val (numerator, denominator) = temps.foldLeft((0.0, 0.0)) {
-        case ((num, den), (loc, temp)) =>
-          val wix = wi(location, loc)
-          (num + wix * temp, den + wix)
-      }
-      assert(denominator != 0.0, "Zero denominator")
-      numerator / denominator
-    }
+    assert(denominator != 0.0, "Zero denominator")
+    numerator / denominator
   }
 
   /**
@@ -61,30 +56,34 @@ object Visualization {
     } else if (len == 1) {
        scale.head._2
     } else {
-      interpolateColorUnsafe(scale.sortBy(_._1).toArray)(value)
+      pixelToColor(interpolatePixel(scale.sortBy(_._1).toArray)(value))
     }
   }
 
-  def interpolateColorUnsafe(sorted: Array[(Double, Color)])(value: Double): Color = {
+  def colorToPixel(color: Color): Pixel = Pixel(color.red, color.green, color.blue, alpha = 255)
+  def pixelToColor(pixel: Pixel): Color = Color(pixel.red, pixel.green, pixel.blue)
+
+  def interpolatePixel(sorted: Array[(Double, Color)])(value: Double): Pixel = {
     val first = sorted.head
     val last = sorted.last
     if (first._1 > value) {
-      first._2
+      colorToPixel(first._2)
     } else if (last._1 < value) {
-      last._2
+      colorToPixel(last._2)
     } else {
       val before = sorted.takeWhile(_._1 < value)
-      if (before.isEmpty) return sorted.head._2
+      if (before.isEmpty) return colorToPixel(first._2)
       val after = sorted.dropWhile(_._1 < value)
-      if (after.isEmpty) return sorted.last._2
+      if (after.isEmpty) return colorToPixel(last._2)
       val a = before.last
       val b = after.head
-      def li(y0: Int, x0: Double, y1: Int, x1: Double, x: Double): Int =
-        round((y0 * (x1 - x) + y1 * (x - x0)) / (x1 - x0)).toInt
-      Color(
-        red   = li(a._2.red,   a._1, b._2.red,   b._1, value),
-        green = li(a._2.green, a._1, b._2.green, b._1, value),
-        blue  = li(a._2.blue,  a._1, b._2.blue,  b._1, value)
+      def interpolateColor(startColor: Int, stopColor: Int, x0: Double, x1: Double, x: Double) =
+        round((startColor * (x1 - x) + stopColor * (x - x0)) / (x1 - x0)).toInt
+      Pixel(
+        interpolateColor(a._2.red,   b._2.red,   a._1, b._1, value),
+        interpolateColor(a._2.green, b._2.green, a._1, b._1, value),
+        interpolateColor(a._2.blue,  b._2.blue,  a._1, b._1, value),
+        255
       )
     }
   }
@@ -106,10 +105,9 @@ object Visualization {
     val colorScale = colors.toArray.sortBy(_._1)
 
     val pixels = locations
-      .toParArray
+      .par
       .map(predictTemperaturePar(temps))
-      .map(interpolateColorUnsafe(colorScale))
-      .map(c => Pixel(c.red, c.green, c.blue, alpha = 255))
+      .map(interpolatePixel(colorScale))
       .toArray
 
     Image(w = 360, h = 180, pixels)
