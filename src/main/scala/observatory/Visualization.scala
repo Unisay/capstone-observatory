7226182de3033ed441c6e5511793feda844f570d
Yuriy Lazarev <unisay@gmail.com>
2017-05-07 17:42:02 +0200

    Visualization up to interpolateColors

--- src/main/scala/observatory/Visualization.scala
+++ src/main/scala/observatory/Visualization.scala
@@ -1,19 +1,51 @@
 package observatory
 
 import com.sksamuel.scrimage.{Image, Pixel}
+import Math._
+
+import scala.collection.parallel.mutable.ParArray
 
 /**
   * 2nd milestone: basic visualization
   */
 object Visualization {
 
+  val earthRadiusMeters = 6372800
+  val powerParameter = 4.0
+
+  def greatCircleDistance(a: Location, b: Location): Double = {
+    val dLat = (b.lat - a.lat).toRadians
+    val dLon = (b.lon - a.lon).toRadians
+    val d = pow(sin(dLat / 2.0), 2.0) + pow(sin(dLon / 2.0), 2.0) * cos(a.lat.toRadians) * cos(b.lat.toRadians)
+    earthRadiusMeters * (2 * asin(sqrt(d)))
+  }
+
   /**
     * @param temperatures Known temperatures: pairs containing a location and the temperature at this location
     * @param location Location where to predict the temperature
     * @return The predicted temperature at `location`
     */
-  def predictTemperature(temperatures: Iterable[(Location, Double)], location: Location): Double = {
-    ???
+  def predictTemperature(temperatures: Iterable[(Location, Double)], location: Location): Double =
+    predictTemperaturePar(temperatures.toParArray)(location)
+
+  def predictTemperaturePar(temps: ParArray[(Location, Double)])(location: Location): Double = {
+    val (closestTemperature, minDistanceMeters) = temps.foldLeft((Double.MaxValue, Double.MaxValue)) {
+      case (same @ (_, minDistance), (nextLocation, nextTemp)) =>
+        val nextDistance = greatCircleDistance(location, nextLocation)
+        if (nextDistance < minDistance) (nextTemp, nextDistance) else same
+    }
+    if (minDistanceMeters <= 1000) {
+      closestTemperature
+    } else {
+      def wi(x: Location, xi: Location): Double = 1.0 / pow(greatCircleDistance(x, xi), powerParameter)
+      val (numerator, denominator) = temps.foldLeft((0.0, 0.0)) {
+        case ((num, den), (loc, temp)) =>
+          val wix = wi(location, loc)
+          (num + wix * temp, den + wix)
+      }
+      assert(denominator != 0.0, "Zero denominator")
+      numerator / denominator
+    }
   }
 
   /**
@@ -22,7 +54,39 @@ object Visualization {
     * @return The color that corresponds to `value`, according to the color scale defined by `points`
     */
   def interpolateColor(points: Iterable[(Double, Color)], value: Double): Color = {
-    ???
+    val scale = points.toVector
+    val len = scale.length
+    if (len == 0) {
+      sys.error("Empty color scale")
+    } else if (len == 1) {
+       scale.head._2
+    } else {
+      interpolateColorUnsafe(scale.sortBy(_._1).toArray)(value)
+    }
+  }
+
+  def interpolateColorUnsafe(sorted: Array[(Double, Color)])(value: Double): Color = {
+    val first = sorted.head
+    val last = sorted.last
+    if (first._1 > value) {
+      first._2
+    } else if (last._1 < value) {
+      last._2
+    } else {
+      val before = sorted.takeWhile(_._1 < value)
+      if (before.isEmpty) return sorted.head._2
+      val after = sorted.dropWhile(_._1 < value)
+      if (after.isEmpty) return sorted.last._2
+      val a = before.last
+      val b = after.head
+      def li(y0: Int, x0: Double, y1: Int, x1: Double, x: Double): Int =
+        round((y0 * (x1 - x) + y1 * (x - x0)) / (x1 - x0)).toInt
+      Color(
+        red   = li(a._2.red,   a._1, b._2.red,   b._1, value),
+        green = li(a._2.green, a._1, b._2.green, b._1, value),
+        blue  = li(a._2.blue,  a._1, b._2.blue,  b._1, value)
+      )
+    }
   }
 
   /**
@@ -31,7 +95,24 @@ object Visualization {
     * @return A 360×180 image where each pixel shows the predicted temperature at its location
     */
   def visualize(temperatures: Iterable[(Location, Double)], colors: Iterable[(Double, Color)]): Image = {
-    ???
+    val locations = for {
+      lat <- 90 until -90 by -1
+      lon <- -180 until 180
+    } yield Location(lat, lon)
+
+    assert(locations.length == 64800, "Wrong number of pixels")
+
+    val temps = temperatures.toParArray
+    val colorScale = colors.toArray.sortBy(_._1)
+
+    val pixels = locations
+      .toParArray
+      .map(predictTemperaturePar(temps))
+      .map(interpolateColorUnsafe(colorScale))
+      .map(c => Pixel(c.red, c.green, c.blue, alpha = 255))
+      .toArray
+
+    Image(w = 360, h = 180, pixels)
   }
 
 }
--- src/test/scala/observatory/VisualizationTest.scala
+++ src/test/scala/observatory/VisualizationTest.scala
@@ -1,13 +1,98 @@
 package observatory
 
 
+import java.lang.Math._
+
+import observatory.Extraction.{locateTemperatures, locationYearlyAverageRecords}
+import observatory.Visualization._
 import org.junit.runner.RunWith
-import org.scalatest.FunSuite
+import org.scalacheck.{Arbitrary, Gen}
+import org.scalatest.{FunSuite, MustMatchers}
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.prop.Checkers
 
 @RunWith(classOf[JUnitRunner])
-class VisualizationTest extends FunSuite with Checkers {
+class VisualizationTest extends FunSuite with Checkers with MustMatchers {
 
+  implicit val arbLocation: Arbitrary[Location] = Arbitrary {
+    for {
+      lat <- Gen.choose(-90.0,  90.0 )
+      lon <- Gen.choose(-180.0, 180.0)
+    } yield Location(lat, lon)
+  }
 
+  test("greatCircleDistance manual") {
+    val x = Location(-87.82640732750883,-102.39289236101074)
+    greatCircleDistance(x, x) must be <= 1.0
+  }
+
+  test("greatCircleDistance manual 2") {
+    val x = Location(50.0359, 5.4253)
+    val y = Location(58.3838, 3.0412)
+    val expDist = 940900
+    val error = abs(greatCircleDistance(x, y) - expDist).toInt
+    error must be <= 1000
+  }
+
+  test("greatCircleDistance") {
+    check((x: Location) => greatCircleDistance(x, x) <= 0.2)
+  }
+
+  test("predict temperature manual") {
+    val x = Location(18.0,-176.0)
+    val y = Location(88.0,-176.0)
+    val z = Location(88.0,-176.0)
+    tempDistProp(x, y, z) mustBe true
+  }
+
+  test("predict temperature") {
+    check((x: Location, y: Location, z: Location) => tempDistProp(x, y, z))
+  }
+
+  private def tempDistProp (x: Location, y: Location, z: Location) = {
+    val tempX = 10.0
+    val tempY = 20.0
+    val temperatures = List((x, tempX), (y, tempY))
+    val tempZ = predictTemperature(temperatures, z)
+    val tempZX = abs(tempX - tempZ)
+    val tempZY = abs(tempY - tempZ)
+    val zx = greatCircleDistance(z, x)
+    val zy = greatCircleDistance(z, y)
+    if (abs(zx - zy) <= 0.2) {
+      tempZX <= 1.0 && tempZY <= 1.0
+    } else {
+      if (zx < zy) {
+        tempZX < tempZY
+      } else {
+        tempZX > tempZY
+      }
+    }
+  }
+
+  test("interpolateColor") {
+    val scale = List(
+      (0.0,           Color(255, 0, 0)),
+      (2.147483647E9, Color(0, 0, 255))
+    )
+    val value = 1.0737418235E9
+    val result = interpolateColor(scale, value)
+    result mustEqual Color(128, 0, 128)
+  }
+
+  test("visualise") {
+    val temps = locateTemperatures(year = 1982, "/stations.csv", "/1982.csv")
+    val temperatures = locationYearlyAverageRecords(temps)
+    val colors = Vector(
+      60.0  -> Color(255, 255, 255),
+      32.0  -> Color(255, 0,   0  ),
+      12.0  -> Color(255, 255, 0  ),
+      0.0   -> Color(0,   255, 255),
+      -15.0 -> Color(0,   0,   255),
+      -27.0 -> Color(255, 0,   255),
+      -50.0 -> Color(33,  0,   107),
+      -60.0 -> Color(0,   0,   0  )
+    )
+    val image = visualize(temperatures, colors)
+    image.output("/tmp/image.png")
+  }
 }
